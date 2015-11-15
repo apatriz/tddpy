2e59b083eb2166e2b815f745c768af9d6baba84f
apatriz <patrizio@msu.edu>
2015-11-14 21:52:36 -0500

    Front page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy my bf a pig' for row in rows)
+			any(row.text == '1: Buy my bf a pig' for row in rows),
+			"New to-do item did not appear in table"
 		)
 
 		# There is still a text box inviting her to add another item.She
