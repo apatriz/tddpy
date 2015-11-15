c731a1533619f6a929b7938300b9a7d58aa79981
apatriz <patrizio@msu.edu>
2015-11-15 18:31:49 -0500

    Can now handle one list item. Ready for refactor...

--- functional_tests.py
+++ functional_tests.py
@@ -37,20 +37,27 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy my bf a pig' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy my bf a pig', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item.She
 		# enters "Give pig a sweater" (noone likes a cold pig)
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Give pig a sweater')
+		inputbox.send_keys(Keys.ENTER)
 
 		#The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy my bf a pig', [row.text for row in rows])
+		self.assertIn(
+			'2: Give pig a sweater' ,
+			[row.text for row in rows]
+		)
 
 		#Mini wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect
+		self.fail('Finish the test!')
 
 		# She visits that URL - her to-do list is still there.
 
