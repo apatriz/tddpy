dc6dc95aded08bfa59826f0a480a5fcc31316e57
apatriz <patrizio@msu.edu>
2015-11-16 21:20:36 -0500

    Refactored FT

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Mini is always forgetting things. She heard about this cool
 		# online to-do app. She checks it out...
@@ -32,12 +38,9 @@ class NewVisitorTest(unittest.TestCase):
 		# She types "Buy my bf a pig" into a text box
 		inputbox.send_keys('Buy my bf a pig')
 		# When she hits enter, the page updates, and now the page lists
-		# "1: Buy my bf a pig" as an item in a to-do list
+		# "1: Buy my bf a pig" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
-
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy my bf a pig', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy my bf a pig')
 
 		# There is still a text box inviting her to add another item.She
 		# enters "Give pig a sweater" (noone likes a cold pig)
@@ -46,14 +49,8 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		#The page updates again, and now shows both items on her list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy my bf a pig', [row.text for row in rows])
-		self.assertIn(
-			'2: Give pig a sweater' ,
-			[row.text for row in rows]
-		)
-
+		self.check_for_row_in_list_table('1: Buy my bf a pig')
+		self.check_for_row_in_list_table('2: Give pig a sweater')
 		#Mini wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect
