c539a4384681dd151d439a323eca27f54cabde5f
apatriz <patrizio@msu.edu>
2015-11-16 23:41:00 -0500

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
+from lists.models import Item
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -31,3 +31,22 @@ class HomePageTest(TestCase):
 		)
 		self.assertEqual(response.content.decode(),expected_html)
 
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
