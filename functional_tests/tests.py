9343e20dbe9c5dc7379307cf2d87111060907053
apatriz <patrizio@msu.edu>
2015-11-18 22:03:20 -0500

    Added functional tests for creating unique URLs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import unittest
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -38,9 +39,13 @@ class NewVisitorTest(LiveServerTestCase):
 
 		# She types "Buy my bf a pig" into a text box
 		inputbox.send_keys('Buy my bf a pig')
-		# When she hits enter, the page updates, and now the page lists
-		# "1: Buy my bf a pig" as an item in a to-do list table
+
+		# When she hits enter, she is taken to a new URL,
+		# and now the page lists "1: Buy my bf a pig" as an item
+		# in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
+		mini_list_url = self.browser.current_url
+		self.assertRegex(mini_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy my bf a pig')
 
 		# There is still a text box inviting her to add another item.She
@@ -52,12 +57,37 @@ class NewVisitorTest(LiveServerTestCase):
 		#The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1: Buy my bf a pig')
 		self.check_for_row_in_list_table('2: Give pig a sweater')
-		#Mini wonders whether the site will remember her list. Then she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect
-		self.fail('Finish the test!')
 
-		# She visits that URL - her to-do list is still there.
+		# Now a new user, Francis, comes along to the site.
+
+		## We use a new browser session to make sure that no information
+		## of Mini's is coming through from cookies etc.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# Francis visits the home page. There is no sign of Mini's
+		# list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy my bf a pig', page_text)
+		self.assertNotIn('Give pig a sweater', page_text)
+
+		# Francis starts a new list by entering a new item. He
+		# is less interesting than Mini...
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, mini_list_url)
+
+		# Again, there is no trace of Mini's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy my bf a pig', page_text)
+		self.assertIn('Buy milk', page_text)
+
 
 		# Satisfied, she goes back to sleep
 
