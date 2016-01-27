b62e69c70b894c60b31979e9275cf207950783ea
apatriz <patrizio@msu.edu>
2016-01-27 00:00:02 -0500

    started form uniqueness validation. read up on model and form validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,10 +1,7 @@
-from django.core.urlresolvers import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.utils.html import escape
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-
+from unittest import skip
 from lists.models import Item, List
 
 
@@ -95,6 +92,19 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+                                    '/lists/%d/' % (list1.id, ),
+                                    data={'text': 'textey'}
+                                    )
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
