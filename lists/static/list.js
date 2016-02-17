66369f2cd8858fb6844c60ac3a78b21c7775e7b1
apatriz <patrizio@msu.edu>
2016-02-16 23:36:44 -0500

    added document ready boilerplate. TODO: hide error on click as well

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,7 @@
-$('input').on('keypress', function(){
-    $('.has-error').hide();
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
+
 
