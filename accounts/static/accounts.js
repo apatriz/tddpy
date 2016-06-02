784b62dbf673168e4a6a4f5a7b33821e87208111
apatriz <patrizio@msu.edu>
2016-06-01 22:53:09 -0700

    continue at 'using a sinon.js mock to check we call the API correctly'

---
+++ accounts/static/accounts.js
@@ -0,0 +1,11 @@
+var initialize = function(navigator){
+    $('#id_login').on('click', function(){
+        navigator.id.request();
+    });
+};
+
+window.Superlists = {
+        Accounts: {
+            initialize: initialize
+        }
+    };
--- superlists/settings.py
+++ superlists/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
