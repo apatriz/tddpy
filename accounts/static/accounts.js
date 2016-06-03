8a355b0cc30bf388644981acba4e07235bd47f10
apatriz <patrizio@msu.edu>
2016-06-02 22:05:56 -0700

    continue at 'And here's how we test the onlogin callback'

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,6 +1,10 @@
-var initialize = function(navigator){
+var initialize = function(navigator, user, token, urls){
     $('#id_login').on('click', function(){
         navigator.id.request();
+    });
+
+    navigator.id.watch({
+        loggedInUser: user
     });
 };
 
