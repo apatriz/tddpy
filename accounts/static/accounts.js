49ca14272ec658451a8434a6f59ee846e9030af5
apatriz <patrizio@msu.edu>
2016-06-04 12:45:49 -0700

    finished client-side authentication (Ch.15)

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -4,7 +4,16 @@ var initialize = function(navigator, user, token, urls){
     });
 
     navigator.id.watch({
-        loggedInUser: user
+        loggedInUser: user,
+        onlogin: function(assertion){
+            $.post(
+                   urls.login,
+                   {assertion: assertion, csrfmiddlewaretoken: token}
+            )
+                    .done(function() {window.location.reload(); })
+                    .fail(function() {navigator.id.logout(); });
+        },
+        onlogout: function(){}
     });
 };
 
